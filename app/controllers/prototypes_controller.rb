647e03510ba4edcd453cce8f882c9f1405c36ae4
SatoruItou <76100945+SatoruItou@users.noreply.github.com>
2021-02-25 22:00:54 +0900

    Protospace-32345

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, expect: [:index, :new, :show]
+  before_action :authenticate_user!, expect: [:index, :new, :show] 
   before_action :move_to_index, expect: [:index, :edit]
   
   def index
