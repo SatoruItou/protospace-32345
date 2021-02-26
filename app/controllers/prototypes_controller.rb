0221eddc599a261b68194f3abd5b6a3154da9cc3
SatoruItou <76100945+SatoruItou@users.noreply.github.com>
2021-02-26 21:53:16 +0900

    Protospace-32345 fix

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show] 
-  before_action :move_to_index, except: [:index, :edit]
+  
   
   def index
     @prototypes = Prototype.all
@@ -54,9 +54,6 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:image, :title, :catch_copy, :concept).merge(user_id: current_user.id)
   end
 
-  def move_to_index 
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+ 
+
 end
